a681375e4dfd75b27712e235188d8cea6b73dc5f
Sam Toohey <sam.toohey@gmail.com>
2021-07-12 21:55:41 +1000

    CSP adjustments
    adjustments made for CSP policy and cleaner execution of inline code

---
+++ script.js
@@ -0,0 +1,9 @@
+alertButton = () => { 
+    let str = "http://";
+    str.replace("http://", "https://")
+    alert('Output of domains')
+  }
+  document.addEventListener('DOMContentLoaded', eventList = () => {
+    document.getElementById('runevent')
+      .addEventListener('click', alertButton);
+  });
