2e78a509a766be9e7cdaf10d2b392994785852b3
Sam Toohey <sam.toohey@gmail.com>
2021-07-14 16:23:49 +1000

    class adjustment

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+  //Tmeplate literal, so concatinate the values of the `labelIsNormal` class and merge with the output. 
+mergeStrings = () => {
+  document.addEventListener('DOMContentLoaded', mergerFunction = () => {
+    document.getElementsByClassName('labelIsNormal')
+    
+  })
+}
+
+//Event Listener that fires when the button is pressed under the domain boxes 
 alertButton = () => { 
     let str = "http://";
     str.replace("http://", "https://")
